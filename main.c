3e1a2dc56d6a8562416ff1104acc54018b7bed82
ffarigg <vashinskayaa@yandex.ru>
2024-05-21 18:43:04 +0300

    A rectangular matrix is given. Arrange the columns of the matrix by non-decreasing the minimum elements of the columns

--- main.c
+++ main.c
@@ -1,17 +1,14 @@
 #include "libs/data_structures/matrix/matrix.h"
-
-int getMax(int *a, int n) {
-    int max = a[0];
+int getMin(int *a, int n) {
+    int min = a[0];
     for (int i = 1; i < n; i++)
-        if (a[i] > max)
-            max = a[i];
-    return max;
+        if (a[i] < min)
+            min = a[i];
+    return min;
 }
-
 void sortRowsByMinElement(matrix m) {
-    insertionSortRowsMatrixByRowCriteria(m, getMax);
+    insertionSortColsMatrixByColCriteria(m, getMin);
 }
-
 int main() {
     int nRows, nCols;
     scanf("%d %d", &nRows, &nCols);
