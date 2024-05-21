3cbecd89dca41aa774eade520a3e60f86986b468
ffarigg <vashinskayaa@yandex.ru>
2024-05-21 19:29:49 +0300

    A rectangular matrix is given. Let's call a pseudodiagonal the set of elements of this matrix lying on a straight line parallel to a straight line containing the elements. Find the sum of the maximum elements of all pseudo-diagonals of a given matrix

--- main.c
+++ main.c
@@ -1,32 +1,44 @@
 #include "libs/data_structures/matrix/matrix.h"
 
-matrix mulMatrices(matrix m1, matrix m2) {
-    assert(m1.nCols == m2.nRows);
-    matrix m3 = getMemMatrix(m1.nRows, m2.nCols);
-    for (int i = 0; i < m3.nRows; i++)
-        for (int j = 0; j < m3.nCols; j++) {
-            m3.values[i][j] = 0;
-            for (size_t k = 0; k < m1.nCols; k++)
-                m3.values[i][j] += m1.values[i][k] * m2.values[k][j];
-        }
-    return m3;
+int getSum(int *a, int n) {
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += a[i];
+    return sum;
 }
 
-bool isMutuallyInverseMatrices(matrix m1, matrix m2) {
-    matrix m3 = mulMatrices(m1, m2);
-    return isEMatrix(m3) ? 1 : 0;
+int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+long long findSumOfMaxesOfPseudoDiagonal(matrix m) {
+    int n = m.nRows + m.nCols - 2;
+    int maxesOfPseudoDiagonal[n];
+    for (size_t i = 0; i < n; i++)
+        maxesOfPseudoDiagonal[i] = 0;
+    int indexPseudoDiagonalElement;
+    for (int i = 0; i < m.nRows; i++)
+        for (int j = 0; j < m.nCols; j++)
+            if (j != i) {
+                if (i > j)
+                    indexPseudoDiagonalElement = j - i + m.nRows - 1;
+                else
+                    indexPseudoDiagonalElement = j - i + m.nRows - 2;
+                maxesOfPseudoDiagonal[indexPseudoDiagonalElement] =
+                        max(maxesOfPseudoDiagonal[indexPseudoDiagonalElement],
+                            m.values[i][j]);
+            }
+    return getSum(maxesOfPseudoDiagonal, n);
 }
 
 int main() {
     int nRows1, nCols1;
     scanf("%d %d", &nRows1, &nCols1);
-    matrix m1 = getMemMatrix(nRows1, nCols1);
-    inputMatrix(m1);
-    int nRows2, nCols2;
-    scanf("%d %d", &nRows2, &nCols2);
-    matrix m2 = getMemMatrix(nRows2, nCols2);
-    inputMatrix(m2);
-    printf("%d", isMutuallyInverseMatrices(m1, m2));
+    matrix m = getMemMatrix(nRows1, nCols1);
+    inputMatrix(m);
+    printf("%lld", findSumOfMaxesOfPseudoDiagonal(m));
     return 0;
 }
+
+
 
