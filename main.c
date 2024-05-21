ccc20848bc4fb78cb926504621a0d6ff83432128
ffarigg <vashinskayaa@yandex.ru>
2024-05-21 18:58:46 +0300

    If the given square matrix is symmetric, then replace it with a square

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include "libs/data_structures/matrix/matrix.h"
-int getMin(int *a, int n) {
-    int min = a[0];
-    for (int i = 1; i < n; i++)
-        if (a[i] < min)
-            min = a[i];
-    return min;
+matrix mulMatrices(matrix m1, matrix m2) {
+    assert(m1.nCols == m2.nRows);
+    matrix m3 = getMemMatrix(m1.nRows, m2.nCols);
+    for (int i = 0; i < m3.nRows; i++)
+        for (int j = 0; j < m3.nCols; j++) {
+            m3.values[i][j] = 0;
+            for (size_t k = 0; k < m1.nCols; k++)
+                m3.values[i][j] += m1.values[i][k] * m2.values[k][j];
+        }
+    return m3;
 }
-void sortRowsByMinElement(matrix m) {
-    insertionSortColsMatrixByColCriteria(m, getMin);
+
+void getSquareOfMatrixIfSymmetric(matrix *m) {
+    if (isSymmetricMatrix(*m))
+        *m = mulMatrices(*m, *m);
 }
 int main() {
     int nRows, nCols;
@@ -16,7 +22,7 @@ int main() {
     matrix m = getMemMatrix(nRows, nCols);
     inputMatrix(m);
 
-    sortRowsByMinElement(m);
+    getSquareOfMatrixIfSymmetric(&m);
 
     outputMatrix(m);
 
