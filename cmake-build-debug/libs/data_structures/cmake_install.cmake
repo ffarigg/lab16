811211c7fc8c1ea9f4dca6e46e3e74e4d0bd8e0f
ffarigg <vashinskayaa@yandex.ru>
2024-05-21 18:14:44 +0300

    Swap the rows containing the maximum and minimum elements

--- cmake-build-debug/libs/data_structures/cmake_install.cmake
+++ cmake-build-debug/libs/data_structures/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Арина Константиновна/CLionProjects/lab16
+# Install script for directory: C:/Users/Арина Константиновна/CLionProjects/lab16/libs/data_structures
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -37,18 +37,3 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "C:/mingw64/bin/objdump.exe")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
-  include("C:/Users/Арина Константиновна/CLionProjects/lab16/cmake-build-debug/libs/cmake_install.cmake")
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Арина Константиновна/CLionProjects/lab16/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
